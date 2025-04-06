8938c7846dde97a19b18ea22e52850baeaeb1d17
MONISHA M <monisham.22ece@kongu.edu>
2025-04-06 17:20:52 +0500

    Delete venv/venv/venv directory

--- venv/venv/venv/product_agent.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_products_from_db():
-    conn = sqlite3.connect('app/database/ecommerce.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, name, category FROM products")
-    products = [{"id": row[0], "name": row[1], "category": row[2]} for row in cursor.fetchall()]
-    conn.close()
-    return products
